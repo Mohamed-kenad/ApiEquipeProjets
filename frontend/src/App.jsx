67691ca0f8b615f4a697c91b537eb18ee778b85f
mohamed <mohamedkenad@gmail.com>
2025-02-25 22:46:00 +0000

    add react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+
+import axios from 'axios';
+
+function App() {
+  const [personnes, setPersonnes] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/api/personnes')
+    .then((res)=>setPersonnes(res.data.data))
+
+  },[])
+
+
+
+  return (
+    <>
+    <table  className="table table-striped table-hover">
+      <thead className='table-dark'>
+        <tr>
+          <th>id</th>
+          <th>nom</th>
+          <th>prenom</th>
+          <th>telephoe</th>
+          <th>ville</th>
+        </tr>
+      </thead>
+      <tbody>
+        {personnes.map((p,i)=>(
+          <tr key={i}>
+            <td>{p.id}</td>
+            <td>{p.nom}</td>
+            <td>{p.prenom}</td>
+            <td>{p.telephone}</td>
+            <td>{p.ville}</td>
+          </tr>
+        ))}
+        <tr></tr>
+      </tbody>
+    </table>
+
+    </>
+  )
+}
+
+export default App
